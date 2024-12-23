05e3cf1bf8e575820af549168e81972f5cbd228a
Posham Shivani <shivaniposham@gmail.com>
2024-12-23 18:33:32 +0530

    Episode1

---
+++ app.js
@@ -0,0 +1,4 @@
+const head=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{},"I am H1 tag!!")))
+const root=ReactDOM.createRoot(document.getElementById("root"))
+root.render(head)
+     
